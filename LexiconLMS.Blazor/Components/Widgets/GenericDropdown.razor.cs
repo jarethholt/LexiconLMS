9fc12b3395f90727f44639018d6ada225c223828
Jareth Holt <jareth.holt@gmail.com>
2024-07-08 08:52:53 +0200

    feat: Add dropdown widgets
    Add a widget class for handling generic (typed) dropdown menus. Implement this widget for the RoleDropdown menu.

---
+++ LexiconLMS.Blazor/Components/Widgets/GenericDropdown.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace LexiconLMS.Blazor.Components.Widgets;
+
+public partial class GenericDropdown<TValue> : ComponentBase where TValue : struct
+{
+    [Parameter]
+    public required string ForName { get; set; }
+    [Parameter]
+    public string LabelText { get; set; } = string.Empty;
+    [Parameter]
+    public string DisplayName { get; set; } = string.Empty;
+    [Parameter]
+    public string DefaultLabel { get; set; } = string.Empty;
+    [Parameter]
+    public TValue? InitialValue { get; set; } = null;
+    [Parameter]
+    public Dictionary<TValue, string> ValuesDict { get; set; } = [];
+    [Parameter]
+    public EventCallback<TValue> OnValueChanged { get; set; }
+
+    private TValue _selectedValue;
+    public TValue SelectedValue
+    {
+        get => _selectedValue;
+        set
+        {
+            _selectedValue = value;
+            OnValueChanged.InvokeAsync(value);
+        }
+    }
+
+    protected override void OnInitialized()
+    {
+        _selectedValue = (InitialValue is null) ? default : (TValue)InitialValue;
+    }
+}
---
+++ LexiconLMS.Blazor/Components/Widgets/RoleDropdown.razor.cs
@@ -0,0 +1,24 @@
+﻿using LexiconLMS.API.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace LexiconLMS.Blazor.Components.Widgets;
+
+public partial class RoleDropdown : ComponentBase
+{
+    [Parameter]
+    public LMSRole? InitialRole { get; set; } = null;
+    [Parameter]
+    public EventCallback<LMSRole> OnRoleChanged { get; set; }
+
+    private readonly Dictionary<LMSRole, string> RolesDict = [];
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        var roleNames = Enum.GetNames<LMSRole>();
+        var roleValues = (LMSRole[])Enum.GetValuesAsUnderlyingType<LMSRole>();
+        var numRoles = roleNames.Length;
+        for (int i = 0; i < numRoles; i++)
+            RolesDict[roleValues[i]] = roleNames[i];
+    }
+}
